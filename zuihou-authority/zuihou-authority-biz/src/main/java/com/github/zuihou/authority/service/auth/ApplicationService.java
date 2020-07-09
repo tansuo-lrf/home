8d5e440840c141288f315783ac2393c1f8ab92e4
tansuo-lrf <55727056+tansuo-lrf@users.noreply.github.com>
2020-07-09 10:03:38 +0800

    Initial commit

---
+++ zuihou-authority/zuihou-authority-biz/src/main/java/com/github/zuihou/authority/service/auth/ApplicationService.java
@@ -0,0 +1,17 @@
+package com.github.zuihou.authority.service.auth;
+
+import com.github.zuihou.authority.entity.auth.Application;
+import com.github.zuihou.base.service.SuperService;
+
+/**
+ * <p>
+ * 业务接口
+ * 应用
+ * </p>
+ *
+ * @author zuihou
+ * @date 2019-12-15
+ */
+public interface ApplicationService extends SuperService<Application> {
+
+}
---
+++ zuihou-authority/zuihou-authority-biz/src/main/java/com/github/zuihou/authority/service/auth/impl/ApplicationServiceImpl.java
@@ -0,0 +1,25 @@
+package com.github.zuihou.authority.service.auth.impl;
+
+
+import com.github.zuihou.authority.dao.auth.ApplicationMapper;
+import com.github.zuihou.authority.entity.auth.Application;
+import com.github.zuihou.authority.service.auth.ApplicationService;
+import com.github.zuihou.base.service.SuperServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 业务实现类
+ * 应用
+ * </p>
+ *
+ * @author zuihou
+ * @date 2019-12-15
+ */
+@Slf4j
+@Service
+
+public class ApplicationServiceImpl extends SuperServiceImpl<ApplicationMapper, Application> implements ApplicationService {
+
+}
